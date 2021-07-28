a5ab6cacf831d54230da1f329fd51dd4fa56a263
chenhaigang <329206444@qq.com>
2021-07-28 11:10:06 +0800

    增加视图控制器实现OpenBase协议

---
+++ CHGComponent/Core/UIViewController+OpenBase.swift
@@ -0,0 +1,13 @@
+//
+//  UIViewController+OpenBase.swift
+//  UIViewController+OpenBase
+//
+//  Created by dnaer5 on 2021/7/28.
+//
+
+import Foundation
+import UIKit
+
+extension UIViewController: OpenBase {
+    
+}
